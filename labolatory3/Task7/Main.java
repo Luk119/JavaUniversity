28864d8553efdf80dc7992d27f5c6c2f6095dcae
Luk119 <lukasz.kundzicz19@interia.pl>
2024-10-21 18:18:16 +0200

    Lab3 Task7

---
+++ labolatory3/Task7/Main.java
@@ -0,0 +1,15 @@
+package labolatory3.Task7;
+
+//Łukasz Kundzicz
+public class Main extends Person {
+    public static void main(String[] args){
+
+        Person person1 = new Person("Sue", 21);
+        Person person2 = new Person("June", 25);
+        Person person3 = new Person("George", 51);
+        Person person4 = new Person("John", 30);
+
+        person1.printNumberOfPeople();
+    }
+
+}
