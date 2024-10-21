db3f72b7e2cafa1b909283eeb68c268d16729aad
Luk119 <lukasz.kundzicz19@interia.pl>
2024-10-21 18:19:28 +0200

    Lab3 Task1234 Rectangle class

---
+++ labolatory3/Tasks123456_8/Main.java
@@ -0,0 +1,50 @@
+package labolatory3.Tasks123456_8;
+
+//Łukasz Kundzicz
+public class Main extends Rectangle {
+    public static void main(String[] args){
+
+        Rectangle rectangle1 = new Rectangle(3);
+        Rectangle rectangle2 = new Rectangle(1, 3);
+        Rectangle rectangle3 = new Rectangle(9, 7);
+
+        rectangle1.setLength(1);
+        rectangle2.setLength(3);
+        rectangle3.setLength(1);
+
+        rectangle1.setWidth(1);
+        rectangle2.setWidth(3);
+        rectangle3.setWidth(5);
+
+        System.out.println("rectangle1:" + rectangle1.getLength());
+        System.out.println("rectangle2:" + rectangle2.getLength());
+        System.out.println("rectangle3:" + rectangle3.getLength());
+
+        System.out.println("rectangle1:" + rectangle1.getWidth());
+        System.out.println("rectangle2:" + rectangle2.getWidth());
+        System.out.println("rectangle3:" + rectangle3.getWidth());
+
+        rectangle1.print();
+        rectangle2.print();
+        rectangle3.print();
+
+        System.out.println(rectangle1.getArea());
+        System.out.println(rectangle2.getArea());
+        System.out.println(rectangle3.getArea());
+
+        System.out.println(rectangle1.getCircuit());
+        System.out.println(rectangle2.getCircuit());
+        System.out.println(rectangle3.getCircuit());
+
+        rectangle1.print();
+        Rectangle[] array = {rectangle1, rectangle2, rectangle3};
+
+        for(Rectangle p : array){
+            p.print();
+        }
+
+        System.out.println("\n\n");
+        Rectangle r1 = new Rectangle(1,-2, 3,-3);
+        System.out.println(r1.getArea());
+    }
+}
