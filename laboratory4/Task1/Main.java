e132d43c62fba7e05c832fa59dda0fadbddb6126
Lukk19 <lukasz.kundzicz19@interia.pl>
2024-10-28 13:14:55 +0100

    Task 1

---
+++ laboratory4/Task1/Main.java
@@ -0,0 +1,27 @@
+package laboratory4.Task1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Wymierne a = new Wymierne(1, 2);
+        Wymierne b = new Wymierne(-2, 4);
+        Wymierne c = new Wymierne(3, 4);
+        Wymierne d = new Wymierne(5, 6);
+
+        System.out.println("Dodawanie: " + a + " + " + b + " = " + a.dodaj(b));
+        System.out.println("Odejmowanie: " + a + " - " + b + " = " + a.odejmij(b));
+        System.out.println("Mnożenie: " + a + " * " + b + " = " + a.pomnoz(b));
+        System.out.println("Dzielenie: " + a + " / " + b + " = " + a.podziel(b));
+        System.out.println("Odwrotność " + a + " = " + a.odwrotnosc());
+
+        Wymierne licznik = a.dodaj(b);
+        Wymierne mianownik = c.odejmij(d);
+        Wymierne wynik = licznik.podziel(mianownik);
+
+        System.out.println("\nWynik wyrażenia (1/2 + (-2)/4) / (3/4 - 5/6): " + wynik);
+
+        Wymierne e = new Wymierne(0, 4);
+        System.out.println(e.odwrotnosc());
+    }
+}
+
---
+++ laboratory4/Task1/Wymierne.java
@@ -0,0 +1,79 @@
+package laboratory4.Task1;
+
+public class Wymierne {
+    private int l;
+    private int m;
+
+    public Wymierne(int l, int m) {
+        if (m == 0) {
+            throw new ArithmeticException("Mianownik nie może być zerem.");
+        }
+        this.l = l;
+        this.m = m;
+        skroc();
+    }
+
+    private void skroc() {
+        int gcd = nwd(Math.abs(l), Math.abs(m));
+        l /= gcd;
+        m /= gcd;
+        if (m < 0) {
+            l = -l;
+            m = -m;
+        }
+    }
+
+    private int nwd(int a, int b) {
+        return b == 0 ? a : nwd(b, a % b);
+    }
+
+    public Wymierne dodaj(Wymierne other) {
+        int nowyL = l * other.m + other.l * m;
+        int nowyM = m * other.m;
+        return new Wymierne(nowyL, nowyM);
+    }
+
+    public Wymierne odejmij(Wymierne other) {
+        int nowyL = l * other.m - other.l * m;
+        int nowyM = m * other.m;
+        return new Wymierne(nowyL, nowyM);
+    }
+
+    public Wymierne pomnoz(Wymierne other) {
+        int nowyL = l * other.l;
+        int nowyM = m * other.m;
+        return new Wymierne(nowyL, nowyM);
+    }
+
+    public Wymierne podziel(Wymierne other) {
+        if (other.l == 0) {
+            throw new ArithmeticException("Nie można dzielić przez zero.");
+        }
+        int nowyL = l * other.m;
+        int nowyM = m * other.l;
+        return new Wymierne(nowyL, nowyM);
+    }
+
+    public Wymierne odwrotnosc() {
+        if (l == 0) {
+            throw new ArithmeticException("Liczba 0 nie ma odwrotności.");
+        }
+        return new Wymierne(m, l);
+    }
+
+    @Override
+    public String toString() {
+        if (m == 1) {
+            return Integer.toString(l);
+        } else if (Math.abs(l) < m) {
+            return l + "/" + m;
+        } else {
+            int calosci = l / m;
+            int nowyL = Math.abs(l % m);
+            if (nowyL == 0) {
+                return Integer.toString(calosci);
+            }
+            return calosci + " " + nowyL + "/" + m;
+        }
+    }
+}
