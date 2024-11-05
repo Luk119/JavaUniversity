d8f5d64fd3bdda095eaacd05767ab2314d99ab4e
Lukk19 <lukasz.kundzicz19@interia.pl>
2024-11-05 11:12:01 +0100

    Task2 toString

--- laboratory5/Task2/Figura.java
+++ laboratory5/Task2/Figura.java
@@ -1,12 +1,12 @@
 package laboratory5.Task2;
 
 //Łukasz Kundzicz
-interface Figura{
+public interface Figura{
 
     double pole();
     double obwod();
 
-    void wypisz();
+//    void wypisz();
 }
 //implementacja klasy Figura
 class  Prostokat implements Figura{
@@ -27,10 +27,10 @@ class  Prostokat implements Figura{
         return 2 * a + 2* b;
     }
 
-    public void wypisz(){
-        System.out.println("prostkat: ");
-        System.out.println("pole: " + pole());
-        System.out.println("obwod: " + obwod());
+
+    @Override
+    public String toString() {
+        return "prostokat: {" + a + ", " + b + "}\npole: " + pole() + "\nobwod: " + obwod() + "\n";
     }
 }
 
@@ -52,10 +52,9 @@ class Kolo implements Figura{
         return 2 * Math.PI * r;
     }
 
-    public void wypisz(){
-        System.out.println("kolo: ");
-        System.out.println("pole: " + pole());
-        System.out.println("obwod: " + obwod());
+    @Override
+    public String toString() {
+        return "kolo: {" + r +  "}\npole: " + pole() + "\nobwod: " + obwod() + "\n";
     }
 }
 //implementacja klasy Trojkat
@@ -82,9 +81,8 @@ class Trojkat implements Figura{
         return a + b + c;
     }
 
-    public void wypisz(){
-        System.out.println("trojkat: ");
-        System.out.println("pole: " + pole());
-        System.out.println("obwod: " + obwod());
+    @Override
+    public String toString() {
+        return "trojkat: {" + a + ", " + b + ", "+ c +"}\npole: " + pole() + "\nobwod: " + obwod() + "\n";
     }
 }
--- laboratory5/Task2/Main.java
+++ laboratory5/Task2/Main.java
@@ -1,17 +1,23 @@
 package laboratory5.Task2;
 
+import laboratory5.Task3.Figura3D;
+import laboratory5.Task3.Kula;
+import laboratory5.Task3.Prostopadloscian;
+
 public class Main {
     public static void main(String[] args) {
         Figura prostokat = new Prostokat(5, 3);
         Figura kolo = new Kolo(4);
         Figura trojkat = new Trojkat(3, 3, 4, 5);
 
-        prostokat.wypisz();
-        System.out.println();
+        Figura3D kula = new Kula(2);
+        Figura3D prostopadloscian = new Prostopadloscian(3, 2, 3);
 
-        kolo.wypisz();
-        System.out.println();
+        System.out.println(prostokat);
+        System.out.println(kolo);
+        System.out.println(trojkat);
+        System.out.println(kula);
+        System.out.println(prostopadloscian);
 
-        trojkat.wypisz();
     }
 }
