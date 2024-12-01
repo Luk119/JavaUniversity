424554a00c3d26549073eac68d6339a81c32ce68
Lukk19 <lukasz.kundzicz19@interia.pl>
2024-12-01 18:12:15 +0100

    revision1

---
+++ revisionTasks/Main.java
@@ -0,0 +1,22 @@
+package revisionTasks;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+// Task 1
+//        System.out.println("Podaj wyraz do odwrócenia: ");
+//        String input = scanner.nextLine();
+//
+//        StringBuilder sb = new StringBuilder(input);
+//
+//        String reversed = sb.reverse().toString();
+//
+//        System.out.println("reversed input: " + reversed);
+
+
+
+    }
+}
