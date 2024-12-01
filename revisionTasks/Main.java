ebe2b4005fe21383be6c5e6b224bfcd67d1143f2
Lukk19 <lukasz.kundzicz19@interia.pl>
2024-12-01 18:12:48 +0100

    revision 2

--- revisionTasks/Main.java
+++ revisionTasks/Main.java
@@ -16,7 +16,20 @@ public class Main {
 //
 //        System.out.println("reversed input: " + reversed);
 
+//    Task 2
 
+//        System.out.println("Enter a sentence: ");
+//        String sentence = scanner.nextLine();
+//        String[] words = sentence.split(" ");
+//        StringBuilder acronym = new StringBuilder();
+//
+//        for(String word : words){
+//            if(!word.isEmpty()){
+//                acronym.append(word.toUpperCase().charAt(0));
+//            }
+//        }
+//
+//        System.out.println(acronym);
 
     }
 }
