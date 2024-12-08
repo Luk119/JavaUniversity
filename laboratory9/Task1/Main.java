614df41c04761d7b4bbe741745ae95b18119de80
Lukk19 <lukasz.kundzicz19@interia.pl>
2024-12-08 12:34:23 +0100

    Main intervals (test)

--- laboratory9/Task1/Main.java
+++ laboratory9/Task1/Main.java
@@ -1,9 +1,12 @@
 package laboratory9.Task1;
+
+import java.util.Arrays;
+
 // Łukasz Kundzicz
 public class Main {
     public static void main(String[] args) {
-        Przedzial p1 = new Przedzial(1.0, true, 5.0, false);
-        Przedzial p2 = new Przedzial(4.0, true, 7.0, true);
+        Przedzial p1 = new Przedzial(1.0, true, 4.0, false);
+        Przedzial p2 = new Przedzial(2.5, true, 7.0, true);
         Przedzial p3 = new Przedzial(Double.NEGATIVE_INFINITY, false, 2.0, false);
         Przedzial p4 = new Przedzial(Double.NEGATIVE_INFINITY, false, Double.POSITIVE_INFINITY, false);
         Przedzial pusty = Przedzial.pusty();
@@ -14,9 +17,11 @@ public class Main {
         System.out.println("p4: " + p4);
         System.out.println("Pusty: " + pusty);
 
-        System.out.println("\np1 \\/ p2: " + p1.suma(p2));
+        System.out.println("\np1 \\/ p2: " + Arrays.toString(p1.sumaRozlaczna(p2)));
         System.out.println("p1 /\\ p2: " + p1.iloczyn(p2));
-        System.out.println("P1 / P2: " + p1.roznica(p2));
-        System.out.println("Dopełnienie P3: " + p3.dopelnienie());
+        System.out.println("p1 /\\ p3: " + p1.iloczyn(p3));
+        System.out.println("p1 / p2: " + p1.roznica(p2));
+        System.out.println("p1 / p4: " + p1.roznica(p4));
+        System.out.println("Dopełnienie p3: " + p3.dopelnienie());
     }
 }
