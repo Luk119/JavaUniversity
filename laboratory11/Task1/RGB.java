a1427deaa23c3c2f12aa7aa7b7ab5df35c37e4d3
Lukk19 <lukasz.kundzicz19@interia.pl>
2024-12-16 11:52:15 +0100

    RGB class constructor, getters

--- laboratory11/Task1/RGB.java
+++ laboratory11/Task1/RGB.java
@@ -1,4 +1,25 @@
 package laboratory11.Task1;
 
 public class RGB {
+
+    public int red, green, blue;
+
+    public RGB(int red, int green, int blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+
+    public int getRed(){
+        return red;
+    }
+
+    public int getGreen(){
+        return green;
+    }
+
+    public int getBlue(){
+        return blue;
+    }
+
 }
