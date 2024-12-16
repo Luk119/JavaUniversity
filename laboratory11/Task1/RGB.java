0635f5ad1b16fc44cbd71c57e68c67c8b904ced0
Lukk19 <lukasz.kundzicz19@interia.pl>
2024-12-16 22:19:47 +0100

    RGB class

--- laboratory11/Task1/RGB.java
+++ laboratory11/Task1/RGB.java
@@ -1,10 +1,14 @@
 package laboratory11.Task1;
 
+// Łukasz Kundzicz
 public class RGB {
 
     public int red, green, blue;
 
     public RGB(int red, int green, int blue) {
+        if(red > 255 || red < 0 || green > 255 || green < 0 || blue > 255 || blue < 0) {
+            throw new IllegalArgumentException("RGB values should be between 0 and 255.");
+        }
         this.red = red;
         this.green = green;
         this.blue = blue;
@@ -22,4 +26,8 @@ public class RGB {
         return blue;
     }
 
+    @Override
+    public String toString() {
+        return "red: " + red + "\ngreen: " + green + "\nblue: " + blue;
+    }
 }
