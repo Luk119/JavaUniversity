cb245832d42faabb9b18256381d51d83a8555579
Lukk19 <lukasz.kundzicz19@interia.pl>
2025-01-31 17:19:45 +0100

    Main

---
+++ laboratory14/Task1/Main.java
@@ -0,0 +1,10 @@
+package laboratory14.Task1;
+
+import javax.swing.*;
+
+//Łukasz Kundzicz
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(BingoGame::new);
+    }
+}
