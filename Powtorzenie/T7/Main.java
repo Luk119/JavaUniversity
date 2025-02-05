b81b250ae1016e0eb5fdf1f0530072943b4815ea
Lukk19 <lukasz.kundzicz19@interia.pl>
2025-02-05 23:05:17 +0100

    Main

---
+++ Powtorzenie/T7/Main.java
@@ -0,0 +1,10 @@
+package Powtorzenie.T7;
+
+public class Main {
+    public static void main(String[] args) {
+
+        C c = new C(1, 12.6f);
+        System.out.println(c.getA());
+        System.out.println(c.getB());
+    }
+}
